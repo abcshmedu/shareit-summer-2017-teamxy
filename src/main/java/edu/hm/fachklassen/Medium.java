a5227be566fe15938d808f4086477bcb096a8700
Sven Schatter <sven.schatter@gmail.com>
2017-04-25 19:05:07 +0200

    added Medium.java and checkstyle cfg

---
+++ src/main/java/edu/hm/fachklassen/Medium.java
@@ -0,0 +1,41 @@
+package edu.hm.fachklassen;
+
+public class Medium {
+    private final String title;
+
+    public Medium(String title) {
+        if (title == null) {
+            throw new IllegalArgumentException("Title must not be null");
+        }
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Medium{" + "title='" + title + '\'' + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Medium medium = (Medium) o;
+
+        return title.equals(medium.title);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return title.hashCode();
+    }
+}
