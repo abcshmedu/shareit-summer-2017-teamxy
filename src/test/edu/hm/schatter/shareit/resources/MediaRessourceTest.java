7513894d8e0e9deb5453bcbb018e1f9467b70298
Sven Schatter <sven.schatter@gmail.com>
2017-06-17 20:40:16 +0200

    tests

--- src/test/edu/hm/schatter/shareit/resources/MediaRessourceTest.java
+++ src/test/edu/hm/schatter/shareit/resources/MediaRessourceTest.java
@@ -40,7 +40,7 @@ public class MediaRessourceTest {
     }
 
     @Test
-    public void addingBookWithValidTokenReturns200() {
+    public void createBookWithValidTokenReturns200() {
         MediaService mediaServiceMock = mock(MediaService.class);
         when(mediaServiceMock.addBook(MOCK_BOOK)).thenReturn(MediaServiceResult.OK);
         mediaRessource = new MediaRessource(mediaServiceMock, trueTokenChecker());
@@ -48,17 +48,169 @@ public class MediaRessourceTest {
         Response result = mediaRessource.createBook(MOCK_BOOK, MOCK_TOKEN);
 
         assertEquals(200, result.getStatus());
-
-        System.out.println(result);
     }
 
     @Test
-    public void addingBookWithInvalidTokenReturns401() {
+    public void createBookWithInvalidTokenReturns401() {
         MediaService mediaServiceMock = mock(MediaService.class);
         when(mediaServiceMock.addBook(MOCK_BOOK)).thenReturn(MediaServiceResult.OK);
         mediaRessource = new MediaRessource(mediaServiceMock, falseTokenChecker());
 
         Response result = mediaRessource.createBook(null, MOCK_TOKEN);
+
+        assertEquals(401, result.getStatus());
+    }
+
+    @Test
+    public void updateBookWithValidTokenReturns200() {
+        MediaService mediaServiceMock = mock(MediaService.class);
+        when(mediaServiceMock.updateBook("isbn", MOCK_BOOK)).thenReturn(MediaServiceResult.OK);
+        mediaRessource = new MediaRessource(mediaServiceMock, trueTokenChecker());
+
+        Response result = mediaRessource.updateBook(MOCK_BOOK, "isbn", MOCK_TOKEN);
+
+        assertEquals(200, result.getStatus());
+    }
+
+    @Test
+    public void updateBookWithInvalidTokenReturns401() {
+        MediaService mediaServiceMock = mock(MediaService.class);
+        when(mediaServiceMock.updateBook("isbn", MOCK_BOOK)).thenReturn(MediaServiceResult.OK);
+        mediaRessource = new MediaRessource(mediaServiceMock, falseTokenChecker());
+
+        Response result = mediaRessource.updateBook(MOCK_BOOK, "isbn", MOCK_TOKEN);
+
+        assertEquals(401, result.getStatus());
+    }
+
+    @Test
+    public void getBooksWithValidTokenReturns200() {
+        MediaService mediaServiceMock = mock(MediaService.class);
+        when(mediaServiceMock.updateBook("isbn", MOCK_BOOK)).thenReturn(MediaServiceResult.OK);
+        mediaRessource = new MediaRessource(mediaServiceMock, trueTokenChecker());
+
+        Response result = mediaRessource.getBooks(MOCK_TOKEN);
+
+        assertEquals(200, result.getStatus());
+    }
+
+    @Test
+    public void getBooksWithInvalidTokenReturns401() {
+        MediaService mediaServiceMock = mock(MediaService.class);
+        when(mediaServiceMock.updateBook("isbn", MOCK_BOOK)).thenReturn(MediaServiceResult.OK);
+        mediaRessource = new MediaRessource(mediaServiceMock, falseTokenChecker());
+
+        Response result = mediaRessource.getBooks(MOCK_TOKEN);
+
+        assertEquals(401, result.getStatus());
+    }
+
+    @Test
+    public void getBookReturnsExistingBook() {
+        MediaService mediaServiceMock = mock(MediaService.class);
+        when(mediaServiceMock.getBookByISBN("isbn")).thenReturn(new Book("", "", ""));
+        mediaRessource = new MediaRessource(mediaServiceMock, trueTokenChecker());
+
+        Response result = mediaRessource.getBook("isbn", MOCK_TOKEN);
+
+        assertEquals(200, result.getStatus());
+    }
+
+    @Test
+    public void getBookInvalidTokenReturns401() {
+        MediaService mediaServiceMock = mock(MediaService.class);
+        when(mediaServiceMock.getBookByISBN("isbn")).thenReturn(null);
+        mediaRessource = new MediaRessource(mediaServiceMock, falseTokenChecker());
+
+        Response result = mediaRessource.getBook("isbn", MOCK_TOKEN);
+
+        assertEquals(401, result.getStatus());
+    }
+
+    // -----------------------------------------------------------------------------------------
+
+    @Test
+    public void createDiscWithValidTokenReturns200() {
+        MediaService mediaServiceMock = mock(MediaService.class);
+        when(mediaServiceMock.addDisc(MOCK_DISC)).thenReturn(MediaServiceResult.OK);
+        mediaRessource = new MediaRessource(mediaServiceMock, trueTokenChecker());
+
+        Response result = mediaRessource.createDisc(MOCK_DISC, MOCK_TOKEN);
+
+        assertEquals(200, result.getStatus());
+    }
+
+    @Test
+    public void createDiscWithInvalidTokenReturns401() {
+        MediaService mediaServiceMock = mock(MediaService.class);
+        when(mediaServiceMock.addDisc(MOCK_DISC)).thenReturn(MediaServiceResult.OK);
+        mediaRessource = new MediaRessource(mediaServiceMock, falseTokenChecker());
+
+        Response result = mediaRessource.createDisc(MOCK_DISC, MOCK_TOKEN);
+
+        assertEquals(401, result.getStatus());
+    }
+
+    @Test
+    public void updateDiscWithValidTokenReturns200() {
+        MediaService mediaServiceMock = mock(MediaService.class);
+        when(mediaServiceMock.updateDisc("barcode", MOCK_DISC)).thenReturn(MediaServiceResult.OK);
+        mediaRessource = new MediaRessource(mediaServiceMock, trueTokenChecker());
+
+        Response result = mediaRessource.updateDisc(MOCK_DISC, "barcode", MOCK_TOKEN);
+
+        assertEquals(200, result.getStatus());
+    }
+
+    @Test
+    public void updateDiscWithInvalidTokenReturns401() {
+        MediaService mediaServiceMock = mock(MediaService.class);
+        when(mediaServiceMock.updateDisc("barcode", MOCK_DISC)).thenReturn(MediaServiceResult.OK);
+        mediaRessource = new MediaRessource(mediaServiceMock, falseTokenChecker());
+
+        Response result = mediaRessource.updateDisc(MOCK_DISC, "barcode", MOCK_TOKEN);
+
+        assertEquals(401, result.getStatus());
+    }
+
+    @Test
+    public void getDiscsWithValidTokenReturns200() {
+        MediaService mediaServiceMock = mock(MediaService.class);
+        mediaRessource = new MediaRessource(mediaServiceMock, trueTokenChecker());
+
+        Response result = mediaRessource.getDiscs(MOCK_TOKEN);
+
+        assertEquals(200, result.getStatus());
+    }
+
+    @Test
+    public void getDiscWithInvalidTokenReturns401() {
+        MediaService mediaServiceMock = mock(MediaService.class);
+        mediaRessource = new MediaRessource(mediaServiceMock, falseTokenChecker());
+
+        Response result = mediaRessource.getDiscs(MOCK_TOKEN);
+
+        assertEquals(401, result.getStatus());
+    }
+
+    @Test
+    public void getDiscReturnsExistingDisc() {
+        MediaService mediaServiceMock = mock(MediaService.class);
+        when(mediaServiceMock.getDiscByBarcode("barcode")).thenReturn(new Disc("", "", "", 0));
+        mediaRessource = new MediaRessource(mediaServiceMock, trueTokenChecker());
+
+        Response result = mediaRessource.getDiscs("barcode", MOCK_TOKEN);
+
+        assertEquals(200, result.getStatus());
+    }
+
+    @Test
+    public void getDiscInvalidTokenReturns401() {
+        MediaService mediaServiceMock = mock(MediaService.class);
+        when(mediaServiceMock.getDiscByBarcode("barcode")).thenReturn(null);
+        mediaRessource = new MediaRessource(mediaServiceMock, falseTokenChecker());
+
+        Response result = mediaRessource.getDiscs("barcode", MOCK_TOKEN);
 
         assertEquals(401, result.getStatus());
     }
